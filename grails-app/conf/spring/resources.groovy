f1d0c4469d58bb2978b3aa72728d5b3ba7087b5f
leftovers <tyama@xmldo.net>
2010-09-19 12:45:24 +0900

    added bean jmsConnectionFactory

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,9 @@
 // Place your Spring DSL code here
 beans = {
+    jmsConnectionFactory(org.apache.activemq.pool.PooledConnectionFactory){bean ->
+        bean.destroyMethod ="stop"
+        connectionFactory(org.apache.activemq.ActiveMQConnectionFactory) {
+            brokerURL = "tcp://localhost:61616"
+        }
+    }
 }
